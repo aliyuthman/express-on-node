4ccf464fd08a5838bff5a333624d2cf95c111b83
Aliyu Usman <aliyuusman64@gmail.com>
2023-12-03 12:15:10 +0100

    HTTP Get request techniques

--- index.js
+++ index.js
@@ -7,27 +7,33 @@ const app = express();
 //for practice this is stored in memory, usually we are to get this from database
 
 const courses = [
-  {id: 1, name: 'course1'},
-  {id: 2, name: 'course2'},
-  {id: 3, name: 'course3'}
-]
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello, node installed ");
 });
 
 app.get("/api/courses", (req, res) => {
-
-    res.json({message: "Hello, courses"})
-})
+  res.send(courses);
+});
 
 //route paramaters vs query param
 app.get("/api/courses/:urlParams", (req, res) => {
+  // write a logic to find the course with the given id/params
+  const params = parseInt(req.params.urlParams);
 
-  const params = req.params.urlParams
-
-  res.json({message: "Hello, URL Paramer is: " + params})
-})
+  // (courseObj) => courseObj.id === params
+  const course = courses.find((courseObj) => {
+    return courseObj.id === params;
+  });
+  if (!course) {
+    res.status(404).send("No course exist with this id: " + params);
+  }
+  res.send(course);
+});
 
 app.listen(port, () => {
   console.log("Server running on " + port);
