a7342b4284e9ac7f5fb2bfa3e47d3f6e1c2c8ccf
Aliyu Usman <aliyuusman64@gmail.com>
2023-12-04 12:58:25 +0100

    Simulating reading from database with setTimeout() and intro to handling async code

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,4 +1,4 @@
-// demonstration of blocking program - 
+// demonstration of blocking program -
 
 /*
 
@@ -12,10 +12,6 @@ console.log("After")
 
 */
 
-
-
-
-
 //demonstration of non-blocking program
 
 /*
@@ -23,8 +19,42 @@ A non-blocking or asynchronous program
 
 */
 
+console.log("Before");
 
-console.log("Before")
-setTimeout(()=>{}, 2000)//simulating reading from database after 2 secs
-console.log("After")
+console.log("After");
 
+const getUser = (id) => {
+  setTimeout(() => {
+    console.log("Reading database...");
+
+    return {
+      id: id,
+      gitHubUsername: "Aliyu",
+    };
+  }, 2000); //simulating reading from database after 2 secs
+};
+
+
+const user = getUser(1); //the return within the setTime out will be delayed, so since the function getUser has no of it own, the results of the console will always undefined as the setTimeout will be delay for 2sec and getUser is instant.
+console.log(user)
+
+/*
+
+Note: setTimeout() function was used to simulate reading from 
+database. It is normal that reading from database usually delayed, thus, the use setTimeout() to schedule execution of a program.
+*/
+
+
+
+
+//Handling Async code in Node
+
+/*
+
+There are three patterns to deals with async code as follows:
+
+1. Callbacks
+2. Promises
+3. Async/await - syntactical sugar over promises in 2
+
+*/
